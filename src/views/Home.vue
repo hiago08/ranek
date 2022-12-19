27412803db04ea040531f5fb9e7a9ae70a0d1397
hiago <moreira.hiago08@gmail.com>
2022-12-19 16:29:48 -0300

    Implementando componentes de lista de produtos e busca de produtos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,32 @@
 <template>
   <div>
-    <p>Home!</p>
+    <section>
+      <h1>👍 Compre ou Venda 👎</h1>
+      <ProdutosBuscar />
+      <ProdutosLista />
+    </section>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import ProdutosBuscar from "@/components/ProdutosBuscar.vue";
+import ProdutosLista from "@/components/ProdutosLista.vue";
 
 export default {
-  name: 'Home',
-}
+  name: "Home",
+  components: {
+    ProdutosBuscar,
+    ProdutosLista,
+  },
+};
 </script>
+
+<style scoped>
+h1 {
+  font-size: 2rem;
+  color: #87f;
+  margin-top: 40px;
+  text-align: center;
+}
+</style>
