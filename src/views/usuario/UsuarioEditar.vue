62822dbb2f654652c3a447667869af0f211750fd
hiago <moreira.hiago08@gmail.com>
2023-01-14 18:49:07 -0300

    Implementando página de atualização de usuário

--- src/views/usuario/UsuarioEditar.vue
+++ src/views/usuario/UsuarioEditar.vue
@@ -1,10 +1,37 @@
 <template>
-  <p>Lista de editar</p>
+  <section>
+    <UsuarioForm>
+      <button class="btn" @click.prevent="atualizarUsuario">
+        Atualizar Usuário
+      </button>
+    </UsuarioForm>
+  </section>
 </template>
 
 <script>
+import UsuarioForm from "@/components/UsuarioForm.vue";
+import { api } from "@/services.js";
 export default {
   name: "UsuarioEditar",
+  components: {
+    UsuarioForm,
+  },
+  methods: {
+    atualizarUsuario() {
+      api
+        .put(
+          `/usuario/${this.$store.state.usuario.id}`,
+          this.$store.state.usuario
+        )
+        .then(() => {
+          this.$store.dispatch("getUsuario", this.$store.state.usuario.email);
+          this.$router.push({ name: "usuario" });
+        })
+        .catch((error) => {
+          console.log(error.response);
+        });
+    },
+  },
 };
 </script>
 
